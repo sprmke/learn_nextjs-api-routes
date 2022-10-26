c72e6ac20e3c8d9139dcba855ede1d8a170b178a
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-10-26 18:49:03 +0800

    147. creating & using dynamic api routes

---
+++ pages/api/[feedbackId].ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getFeedbackData } from './feedback';
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  switch (req.method) {
+    case 'GET': {
+      // get dynamic route id
+      const { feedbackId } = req.query;
+
+      // get selected feedback data from a file
+      const data = getFeedbackData();
+      const selectedFeedback = data.find(({ id }) => id === feedbackId);
+
+      res.status(200).json({ message: 'Success!', feedback: selectedFeedback });
+      break;
+    }
+    // case 'POST': {
+    //   break;
+    // }
+    // case 'PUT': {
+    //   break;
+    // }
+    // case 'DELETE': {
+    //   break;
+    // }
+    default: {
+      res.status(400).json({ message: 'Invalid request method!' });
+    }
+  }
+};
+
+export default handler;
--- pages/feedback/index.tsx
+++ pages/feedback/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next';
 import { Feedback } from '../../types/feedback';
 import { getFeedbackData } from '../api/feedback';
@@ -5,16 +6,43 @@ import { getFeedbackData } from '../api/feedback';
 const FeedbackPage: NextPage<
   InferGetStaticPropsType<typeof getStaticProps>
 > = ({ feedbackItems }) => {
+  const [selectedFeedback, setSelectedFeedback] = useState<Feedback | null>(
+    null
+  );
+  const getFeedbackHandler = async (id: string) => {
+    const response = await fetch(`/api/${id}`);
+    const data = await response.json();
+    const { feedback }: { feedback: Feedback } = data;
+
+    setSelectedFeedback(feedback);
+  };
+
   return (
     <div>
-      <h2>Feedback items</h2>
-      {feedbackItems.length > 0 && (
-        <ul>
-          {feedbackItems?.map(({ id, email, feedback }) => (
-            <li key={id}>{feedback}</li>
-          ))}
-        </ul>
+      {selectedFeedback && (
+        <div>
+          <h2>Selected Feedback</h2>
+          <p>{selectedFeedback.id}</p>
+          <p>{selectedFeedback.email}</p>
+          <p>{selectedFeedback.feedback}</p>
+        </div>
       )}
+      <hr />
+      <div>
+        <h2>Feedback items</h2>
+        {feedbackItems.length > 0 && (
+          <ul>
+            {feedbackItems?.map(({ id, email, feedback }) => (
+              <li key={id}>
+                {feedback}
+                <button onClick={getFeedbackHandler.bind(null, id)}>
+                  Get Details
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 };
